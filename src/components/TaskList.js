36dc9fb9b5d6250fc1a9572c5d20279cc27d2d3f
elias.caracas <elias.caracas@eggcooperation.com>
2023-03-21 01:03:45 -0300

    add pagination

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,12 +1,19 @@
-import React from "react"
+import React, { useState } from "react"
 import { deleteTask } from "../features/tasks/taskSlice"
 import { useDispatch, useSelector } from "react-redux"
 import { Link } from "react-router-dom"
 import { logout } from "../features/auth/authSlice"
 function TaskList() {
-  const dispatch = useDispatch()
+  const [currentPage, setCurrentPage] = useState(1)
+  const recordsPerPage = 5
+  const lastIndex = currentPage * recordsPerPage
+  const firstIndex = lastIndex - recordsPerPage
 
+  const dispatch = useDispatch()
   const tasks = useSelector((state) => state.tasks.list)
+  const records = tasks.slice(firstIndex, lastIndex)
+  const npages = Math.ceil(tasks.length / recordsPerPage)
+  const numbers = [...Array(npages + 1).keys()].slice(1)
 
   const handleDelete = (id) => {
     dispatch(deleteTask(id))
@@ -19,6 +26,12 @@ function TaskList() {
       })
     )
   }
+
+  const nextPage = () => {}
+
+  const prevPage = () => {}
+
+  const changeCurrentPage = (n) => {}
 
   return (
     <div className="w-4/6">
@@ -35,7 +48,7 @@ function TaskList() {
         <button onClick={handleLogOut}>LogOut</button>
       </header>
 
-      {tasks.map((task) => (
+      {records.map((task) => (
         <div
           key={task.id}
           className="mt-5 flex items-center justify-between p-2"
@@ -88,6 +101,147 @@ function TaskList() {
           </div>
         </div>
       ))}
+
+      <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+        <div className="flex flex-1 justify-between sm:hidden">
+          <a
+            href="#"
+            className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+          >
+            Previous
+          </a>
+          <a
+            href="#"
+            className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+          >
+            Next
+          </a>
+        </div>
+        <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+          <div>
+            <p className="text-sm text-gray-700">
+              Showing <span className="font-medium">{firstIndex + 1}</span> to{" "}
+              <span className="font-medium">{lastIndex}</span> of{" "}
+              <span className="font-medium">{tasks.length}</span> results
+            </p>
+          </div>
+          <div>
+            <nav
+              className="isolate inline-flex -space-x-px rounded-md shadow-sm"
+              aria-label="Pagination"
+            >
+              <a
+                href="#"
+                className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+              >
+                <span className="sr-only" onClick={prevPage}>Previous</span>
+                <svg
+                  className="h-5 w-5"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </a>
+
+              {/* <a
+                href="#"
+                aria-current="page"
+                className="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                1
+              </a>
+              <a
+                href="#"
+                className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+              >
+                2
+              </a>
+              <a
+                href="#"
+                className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
+              >
+                3
+              </a> */}
+
+              {numbers
+                .filter((n, i) => i <= 2)
+                .map((n, i) => (
+                  <a
+                    href="#"
+                    aria-current="page"
+                    className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
+                    key={i}
+                    onClick={() => changeCurrentPage(n)}
+                  >
+                    {n}
+                  </a>
+                ))}
+
+              <span className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700 ring-1 ring-inset ring-gray-300 focus:outline-offset-0">
+                ...
+              </span>
+
+              {numbers
+                .filter((n,i) => i>=npages-3)
+                .map((n, i) => (
+                  <a
+                    href="#"
+                    aria-current="page"
+                    className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
+                    key={i}
+                    onClick={() => changeCurrentPage(n)}
+                  >
+                    {n}
+                  </a>
+                ))}
+
+              {/* <a
+                href="#"
+                className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
+              >
+                8
+              </a>
+              <a
+                href="#"
+                className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+              >
+                9
+              </a>
+              <a
+                href="#"
+                className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+              >
+                10
+              </a> */}
+
+              <a
+                href="#"
+                className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+              >
+                <span className="sr-only" onClick={nextPage}>Next</span>
+                <svg
+                  className="h-5 w-5"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </a>
+            </nav>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
